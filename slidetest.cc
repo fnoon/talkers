b497a5e3b5fdb1c5d5ead53147b810d8562f8f88
Frederick Noon @crake <FrS.Noon@gmail.com>
2018-08-11 15:13:17 -0700

    Tweak formatting.

--- slidetest.cc
+++ slidetest.cc
@@ -7,8 +7,6 @@
 
 #include <iostream>
 
-//using boost::accumulators::accumulator_set;
-//using boost::accumulators::stats;
 using namespace boost::accumulators;
 using std::cout;
 using std::endl;
@@ -17,7 +15,7 @@ using std::endl;
 int
 main (int argc, char* argv[])
 {
-  accumulator_set<int, stats<tag::rolling_sum> > acc(tag::rolling_window::window_size = 3);
+  accumulator_set<int, stats<tag::rolling_sum>> acc(tag::rolling_window::window_size = 3);
 
   acc(1);
   acc(2);
